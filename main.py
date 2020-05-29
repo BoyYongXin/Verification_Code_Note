187799092952060227ec9ae969e410aa6653c004
BoyYongXin <13717827309@163.com>
2020-05-29 16:54:00 +0800

    整理readme图片

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import tesserocr
 image_path = os.path.join(os.path.dirname((os.path.abspath(__file__))), 'images/2.jpg')
 img = Image.open(image_path)
 threshold = 200
+# threshold = 100
 w, h = img.size
-print(w, h)
+
 image = binarizing(img, threshold)
 threshold = 245
 imgs = depoint(image, threshold)
@@ -16,5 +17,5 @@ print(tesserocr.image_to_text(imgs))
 imgs.show()
 
 # import pytesseract
-# content = pytesseract.image_to_string(imgs)  # 解析图片
+# content = pytesseract.image_to_string(image)  # 解析图片
 # print(content)
